f51cdc61bf7712b945afe95afdfb040b8816124e
Nicholas <43795440+Loxodograph@users.noreply.github.com>
2025-09-02 12:58:00 +0000

    First Commit

---
+++ js130/toDoList.js
@@ -0,0 +1,64 @@
+// This class represents a todo item and its associated
+// data: the todo title and a flag that shows whether the
+// todo item is done.
+
+class Todo {
+  static DONE_MARKER = "X";
+  static UNDONE_MARKER = " ";
+
+  constructor(title) {
+    this.title = title;
+    this.done = false;
+
+  }
+
+  toString() {
+    let marker = this.isDone() ? Todo.DONE_MARKER : Todo.UNDONE_MARKER;
+    return `[${marker}] ${this.title}`;
+  }
+
+  markDone() {
+    this.done = true;
+  }
+
+  markUndone() {
+    this.done = false;
+  }
+
+  isDone() {
+    return this.done;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+}
+
+class TodoList {
+  constructor(title) {
+    this.title = title;
+    this.todos = [];
+  }
+
+  add(title) {
+    this.todos.push(title);
+  }
+}
+
+let list = new TodoList("Today's Todos");
+
+console.log(list);
+
+let todo1 = new Todo("Buy milk");
+let todo2 = new Todo("Clean room");
+let todo3 = new Todo("Go to the gym");
+let todo4 = new Todo("Go shopping");
+
+list.add(todo1);
+list.add(todo2);
+list.add(todo3);
+list.add(todo4);
+
+console.log(list);
+
+list.add(1);
