d4dcfc511935c753f8369f5176c8eb7ddb13a53d
Nicholas <43795440+Loxodograph@users.noreply.github.com>
2025-09-04 18:41:14 +0000

    todolist2

--- js130/toDoList.js
+++ js130/toDoList.js
@@ -93,49 +93,50 @@ class TodoList {
     return this.todos.pop();
   }
 
-  removeAt() {
-    this.todos.
+  removeAt(index) {
+    this._validateIndex(index);
+    return this.todos.splice(index, 1);
+  }
+
+  toString() {
+    let title = `-----${this.title}-----`;
+    let list = this.todos.map(todo => todo.toString()).join("\n");
+    return `${title}\n${list}`;
+  }
+
+  forEach(callback) {
+    for (let index = 0; index < this.todos.length; index++) {
+      callback(this.todos[index]);
+    }
+  }
+
+  filter(callback) {
+    let result = [];
+    for (let index = 0; index < this.todos.length; index++) {
+      if (callback(this.todos[index])) {
+        result.push(this.todos[index]);
+      }
+    }
+    return result;
   }
 }
-
-let list = new TodoList("Today's Todos");
-let emptyList = new TodoList("Empty List");
 
 let todo1 = new Todo("Buy milk");
 let todo2 = new Todo("Clean room");
 let todo3 = new Todo("Go to the gym");
 let todo4 = new Todo("Go shopping");
+let todo5 = new Todo("Feed the cats");
+let todo6 = new Todo("Study for Launch School");
+let list = new TodoList("Today's Todos");
 
 list.add(todo1);
 list.add(todo2);
 list.add(todo3);
 list.add(todo4);
+list.add(todo5);
+list.add(todo6);
+todo1.markDone();
+todo5.markDone();
 
-// console.log(list.itemAt(1));
-
-list.markDoneAt(1);
-// console.log(list);
-
-list.markUndoneAt(1);
-// console.log(list);
-
-// console.log(list.isDone()); // false
-
-list.markDoneAt(0);
-list.markDoneAt(1);
-list.markDoneAt(2);
-list.markDoneAt(3);
-// console.log(list.isDone()); // true
-
-list.markUndoneAt(2);
-// console.log(list.isDone()); // false
-
-// Omitted code
-
-console.log(list.shift());
-console.log(list.pop());
-console.log(list);
-
-console.log(emptyList.shift());
-console.log(emptyList.pop());
-console.log(emptyList);
+let doneTodos = list.filter(todo => todo.isDone());
+console.log(doneTodos);
