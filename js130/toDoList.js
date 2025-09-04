1d5088e98cd6d896c736fdb8d492b3aaba4a2d36
Nicholas <43795440+Loxodograph@users.noreply.github.com>
2025-09-04 14:00:12 +0000

    toDoList.js

--- js130/toDoList.js
+++ js130/toDoList.js
@@ -41,13 +41,65 @@ class TodoList {
   }
 
   add(title) {
-    this.todos.push(title);
+    if (title instanceof Todo) {
+      this.todos.push(title);
+    } else {
+      console.log('TypeError: Can only add todo objects');
+    }
+  }
+
+  size() {
+    return this.todos.length;
+  }
+
+  first() {
+    return this.todos[0];
+  }
+
+  last() {
+    return this.todos[this.size() - 1];
+  }
+
+  itemAt(index) {
+    this._validateIndex(index);
+    return this.todos[index];
+  }
+
+  _validateIndex(index) { // _ in name suggests a "private" method
+    if (!(index in this.todos)) {
+      throw new ReferenceError(`invalid index: ${index}`);
+    }
+  }
+
+  markDoneAt(index) {
+    this.itemAt(index).markDone();
+  }
+
+  markUndoneAt(index) {
+    this.itemAt(index).markUndone();
+  }
+
+  isDone() {
+    return this.todos.every(item => {
+      return item.isDone();
+    });
+  }
+
+  shift() {
+    return this.todos.shift();
+  }
+
+  pop() {
+    return this.todos.pop();
+  }
+
+  removeAt() {
+    this.todos.
   }
 }
 
 let list = new TodoList("Today's Todos");
-
-console.log(list);
+let emptyList = new TodoList("Empty List");
 
 let todo1 = new Todo("Buy milk");
 let todo2 = new Todo("Clean room");
@@ -59,6 +111,31 @@ list.add(todo2);
 list.add(todo3);
 list.add(todo4);
 
+// console.log(list.itemAt(1));
+
+list.markDoneAt(1);
+// console.log(list);
+
+list.markUndoneAt(1);
+// console.log(list);
+
+// console.log(list.isDone()); // false
+
+list.markDoneAt(0);
+list.markDoneAt(1);
+list.markDoneAt(2);
+list.markDoneAt(3);
+// console.log(list.isDone()); // true
+
+list.markUndoneAt(2);
+// console.log(list.isDone()); // false
+
+// Omitted code
+
+console.log(list.shift());
+console.log(list.pop());
 console.log(list);
 
-list.add(1);
+console.log(emptyList.shift());
+console.log(emptyList.pop());
+console.log(emptyList);
