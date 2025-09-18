d79641299ead328d3c2f8ec4770aea9fe099d933
Nicholas <43795440+Loxodograph@users.noreply.github.com>
2025-09-18 18:27:11 +0000

    diamond.js

---
+++ challenges/diamond/diamond.js
@@ -0,0 +1,11 @@
+//Create a diamond using a given letter.
+
+//First row is A, last Row is A
+// All rows, except for first and last contain 2 identical letters
+// Diamond is symmetrical
+//
+class Diamond {
+
+}
+
+module.exports = Diamond
