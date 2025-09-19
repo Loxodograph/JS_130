922826980df24400cbfc1b15fb9a45a7d9f11585
Nicholas <43795440+Loxodograph@users.noreply.github.com>
2025-09-19 18:34:40 +0000

    beersong junk

---
+++ regex/regex.js
@@ -0,0 +1,14 @@
+// let mystr = 'cast';
+
+// if (mystr.match(/s/)) {
+//   console.log("Matched 's'");
+// }
+
+// if (mystr.match(/x/)) {
+//   console.log("Matched x");
+// }
+
+let text = "1 2 3 \t 4 5 6";
+if (text.match(/\t/)) {
+  console.log("has tab");
+}
