e2804efd9ddecfd01dd562062120b8a66036955d
Loxodograph <paldino.nicholas@gmail.com>
2025-09-19 19:18:28 -0400

    beersong.js

--- challenges/beerSong/beerSong.js
+++ challenges/beerSong/beerSong.js
@@ -1,4 +1,3 @@
-"use strict";
 /*
 Problem
 Write a program that generates the lyrics to 99 bottles of beer
@@ -15,44 +14,62 @@ create a method that takes up to 2 verse numbers and
 outputs the verses until the second number is reached
 */
 
-// class BeerSong {
+"use strict";
 
-//   static BEER = ["bottle", "bottles"];
-//   static AMOUNT = ["it", "one"];
 
-//   static verse(start, end) {
-//     let finalVerse = this.singleVerse(start);
-//     for (let currentVerse = start - 1; currentVerse >= end; currentVerse--) {
-//       if (currentVerse === 0) {
-//         finalVerse += this.finalVerse();
-//       } else {
-//         finalVerse += this.singleVerse(currentVerse);
-//       }
-//     }
-//     return finalVerse;
-//   }
+class BeerSong {
+  static BEER_WORDS = {
+    0: {
+      amount: "one",
+      bottle: "bottles"
+    },
+    1: {
+      amount: "it",
+      bottle: "bottle",
+    },
+    2: {
+      amount: "one",
+      bottle: "bottles"
+    }
+  }
 
-//   static singleVerse(number) {
-//     let bottle = this.BEER[0];
-//     let amount = this.AMOUNT[0];
-//     let remaining = number - 1 || "no more";
-//     if (number > 1) amount = this.AMOUNT[1];
-//     if (number > 1) bottle = this.BEER[1];
-//     if (number - 1 === 1) bottle = this.BEER[0];
-//     if (number - 1 === 0) amount = "no more";
-//     let verse = `${number} ${bottle} of beer on the wall, ${number} ${bottle} of beer.\nTake ${amount} down and pass it around, ${remaining} ${bottle} of beer on the wall.\n`;
-//     return verse;
-//   }
+  static verse(bottlesOfBeer) {
+    let access = bottlesOfBeer;
+    if (bottlesOfBeer > 2) access = 2;
 
-//   static finalVerse() {
-//     return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n";
-//   }
+    let number = bottlesOfBeer;
 
-//   static lyrics() {
-//     return BeerSong.verse(99, 0);
-//   }
-// }
+    let bottle = this.BEER_WORDS[access].bottle;
+    let amount = this.BEER_WORDS[access].amount;
 
-// console.log(BeerSong.verse(2, 0));
+    let remaining = (number - 1);
+    if (remaining < 0) {
+      return this.finalVerse();
+    }
 
-// module.exports = BeerSong;
+    let verse = `${number} ${bottle} of beer on the wall, ${number} ${bottle} of beer.\nTake ${amount} down and pass it around, ${remaining === 0 ? "no more" : remaining} ${remaining === 1 ? "bottle" : "bottles"} of beer on the wall.\n`;
+    return verse;
+  }
+
+  static finalVerse() {
+    let remaining = "no more";
+    let number = "No more";
+    let bottle = this.BEER_WORDS[0].bottle;
+    return `${number} ${bottle} of beer on the wall, ${remaining} ${bottle} of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n`;
+  }
+
+  static verses(start, end) {
+    let lyrics = [];
+    for (let current = start; current >= end; current--) {
+      lyrics.push(this.verse(current));
+    }
+    return lyrics.join("\n");
+  }
+
+  static lyrics() {
+    return this.verses(99, 0);
+  }
+}
+
+
+module.exports = BeerSong;
