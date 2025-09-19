eebba88bfe5090d80661641083256b8fd0eeddb2
Nicholas <43795440+Loxodograph@users.noreply.github.com>
2025-09-19 13:08:24 +0000

    x

--- challenges/diamond/diamond.js
+++ challenges/diamond/diamond.js
@@ -7,17 +7,21 @@
 class Diamond {
 
   static ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
   static makeDiamond(letter) {
     if (letter === "A") return this.returnA();
+
     let letterCode = Diamond.ALPHABET.indexOf(letter.toUpperCase()) + 1;
-    let alphabetArray = Diamond.ALPHABET.slice(0, letterCode);
-    let diamondLineArray = [];
+    let alphabetArray = Diamond.ALPHABET.slice(0, letterCode)
+      .concat(Diamond.ALPHABET.slice(0, (letterCode - 1))
+        .reverse());
+
     let totalWidth = (2 * (letterCode - 1)) + 1;
-    diamondLineArray.push(...(this.forwardIteration(alphabetArray,
-      this.returnLetter, totalWidth)));
-    diamondLineArray.push(...(this.backwardsIteration(alphabetArray,
-      this.returnLetter, totalWidth)));
-    console.log(diamondLineArray);
+
+    let diamondLineArray = alphabetArray.map(char => {
+      return this.makeRow(char, totalWidth);
+    });
+
     return diamondLineArray.join("");
   }
 
@@ -29,7 +33,7 @@ class Diamond {
     return " ".repeat(spaces) + "A" + " ".repeat(spaces) + "\n";
   }
 
-  static returnLetter(letter, totalWidth) {
+  static makeRow(letter, totalWidth) {
     let letterCode = Diamond.ALPHABET.indexOf(letter.toUpperCase()) + 1;
     let totalInnerSpaces = Math.floor((2 * (letterCode - 2)) + 1);
     let totalOuterSpaces = Math.ceil(((totalWidth -
@@ -37,23 +41,6 @@ class Diamond {
 
     if (letter === "A") return Diamond.returnAWithSpaces((Math.floor((totalWidth / 2))));
     return " ".repeat(totalOuterSpaces) + letter + " ".repeat(totalInnerSpaces) + letter + " ".repeat(totalOuterSpaces) + "\n";
-  }
-
-  static forwardIteration(array, callback, argument) {
-    let resultArray = [];
-    for (let index = 0; index < array.length; index++) {
-      resultArray.push(callback(array[index], argument));
-    }
-    return resultArray;
-  }
-
-  static backwardsIteration(array, callback, argument) {
-    let resultArray = [];
-    for (let index = array.length - 2;
-      index >= 0; index--) {
-      resultArray.push(callback(array[index], argument));
-    }
-    return resultArray;
   }
 }
 
