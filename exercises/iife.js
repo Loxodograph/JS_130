31f718bcb1b2fc5859b978003c997826e6e587f1
Nicholas <43795440+Loxodograph@users.noreply.github.com>
2025-09-25 18:17:38 +0000

    x

---
+++ exercises/iife.js
@@ -0,0 +1,35 @@
+// var sum = 0;
+// sum += 10;
+// sum += 31;
+
+// let numbers = [1, 7, -3, 3];
+
+// sum += (function (numbers) {
+//   return numbers.reduce((sum, number) => {
+//     sum += number;
+//     return sum;
+//   }, 0);
+// }
+// )(numbers);  // ?
+
+// console.log(sum);
+
+// (function (number) {
+//   while (number >= 0) {
+//     console.log(number);
+//     number--;
+//   }
+// })(7);
+
+// let bar = (function foo(start) {
+//   let prod = start;
+//   return function (factor) {
+//     prod *= factor;
+//     return prod;
+//   };
+// })(2);
+
+// let result = bar(3);
+// result += bar(4);
+// result += bar(5);
+// console.log(result);
